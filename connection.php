d4b4996c5f23c1b1ae5603aa716eab632847e300
= <=>
2023-09-18 03:04:50 +0530

    commit done

--- connection.php
+++ connection.php
@@ -1,17 +1,21 @@
 <?php
-
+// Turn off error reporting to suppress PHP errors
 error_reporting(0);
 
-$server_name = "localhost";
-$username = "root";
-$password = "";
-$db_name = "employee";
+// Database connection parameters
+$server_name = "localhost"; // MySQL server hostname
+$username = "root"; // MySQL username
+$password = ""; // MySQL password (leave empty for no password)
+$db_name = "employee"; // database name
 
+// Establish a MySQL database connection
 $connection = mysqli_connect($server_name, $username, $password, $db_name);
 
+// Check if the connection was successful
 if($connection){
-    echo "Connection established successfully to the database";
+    // Connection successful, no action required here
 } else{
-    echo "Failed to connect to the database ".mysqli_connect_error();
+    // Connection failed, display an error message
+    echo "Failed to connect to the database " . mysqli_connect_error();
 }
 ?>
